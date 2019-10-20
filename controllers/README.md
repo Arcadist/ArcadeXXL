006ea17c0694d5431f0ae27c30e67e4a1038e39a
patrick <paddel10@hotmail.com>
2019-10-20 15:04:02 +0200

    controllers.ino defines

--- controllers/README.md
+++ controllers/README.md
@@ -11,6 +11,11 @@ Displaying button/LED numbering:
 | 1 - 6 | **Player 1**, Button 1 = Select |
 | 8 - 14 | **Player 2**, Button 8 = Select |
 | 15 | **Hotkey** |
-| 15 | TBD |
+| 16 | TBD |
 | 17 - 23 | **Player 3**, Button 17 = Select |
 | 24 - 30 | **Player 4**, Button 24 = Select |
+
+LEDs are driven by an ESP32 controllerboard using [FastLED](https://github.com/FastLED/FastLED) library.
+
+### References
+- [FastLED - Pixel reference](https://github.com/FastLED/FastLED/wiki/Pixel-reference)
--- controllers/controllers.ino
+++ controllers/controllers.ino
@@ -1,16 +1,40 @@
-
 #include <FastLED.h>
 
-#define NUM_LEDS = 30;
-#define DATA_PIN 6
+#define NUM_LEDS 30
+#define DATA_PIN 12
 
-#define PLAYER1_COLOR = 0xBF302F; // red
-#define PLAYER2_COLOR = 0xE8C639; // yellow
-#define PLAYER3_COLOR = 0x9566AC; // purple
-#define PLAYER4_COLOR = 0x68BF6F; // green
+#define PLAYER1_COLOR 0xBF302F // red
+#define PLAYER2_COLOR 0xE8C639 // yellow
+#define PLAYER3_COLOR 0x9566AC // purple
+#define PLAYER4_COLOR 0x68BF6F // green
 
-#define PLAYER_SELECT_COLOR = 0x; // player select 1 - 4
-#define SELECT_COLOR = 0x; // start, select button
+#define PLAYER_SELECT_COLOR CRGB::White; // player select 1 - 4
+#define SELECT_COLOR CRGB::White; // start, select button
+
+// button layout
+// player 1 (start #2)
+//  3 | 5 | 7
+//  2 | 4 | 6
+// player 2 (start #9)
+// 10 | 12 | 14
+//  9 | 11 | 13
+// player 3 (start #18)
+// 19 | 21 | 23
+// 18 | 20 | 22
+// player 4 (start #25)
+// 26 | 28 | 14
+// 25 | 27 | 13
+
+int RAINBOW_COLOR_PATTERN[] = {
+    CRGB::Red,
+    CRGB::Orange,
+    CRGB::Yellow,
+    CRGB::Green,
+    CRGB::Aqua,
+    CRGB::Blue,
+    CRGB::Purple,
+    CRGB::Pink
+};
 
 CRGB leds[NUM_LEDS];
 
